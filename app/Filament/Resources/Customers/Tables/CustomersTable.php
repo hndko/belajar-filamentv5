8cefae86cdefe0543a06e18a654f2461d3a56e9e
hndko <hd.doko22@gmail.com>
2026-01-26 17:55:23 +0700

    feat: Implement Filament resources for Customer and Product models, including migrations, pages, tables, and forms.

---
+++ app/Filament/Resources/Customers/Tables/CustomersTable.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Resources\Customers\Tables;
+
+use Filament\Tables\Table;
+use Filament\Actions\EditAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Tables\Columns\TextColumn;
+
+class CustomersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
